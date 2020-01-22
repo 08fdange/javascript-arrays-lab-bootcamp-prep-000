bb46d0a47d82d74ab85d694f7fbb68057b16251e
Frank  D'Angelo <08fdange@gmail.com>
2020-01-22 19:50:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
-  return kittens.push(name)
+  return kittens.push(name);
 }
 
 function destructivelyPrependKitten(name){
-  return kittens.unshift(name)
+  return kittens.unshift(name);
 }
 
 function destructivelyRemoveLastKitten(name) {
-  return kittens.pop(name)
+  return kittens.pop(name);
 }
 
 function destructivelyRemoveFirstKitten(name) {
-  return kittens.shift(name)
+  return kittens.shift(name);
 }
 
 function appendKitten(name) {
-  return [...kittens, name]
+  return [...kittens, name];
 }
 
 function prependKitten(name) {
-  return [name,...kittens]
+  return [name,...kittens];
 }
 
 function removeLastKitten() {
@@ -30,6 +30,6 @@ function removeLastKitten() {
 }
 
 function removeFirstKitten() {
-  return kittens.slice(1,4)
+  return kittens.slice(1,4);
 }
   
